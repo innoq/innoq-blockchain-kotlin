ecb81fce037fe5ae92544735cab8b953e22e970c
Michael Neuweiler <michael_neuweiler@yahoo.com>
2018-04-19 15:36:08 +0200

    alternative approach to create json string in miner

--- src/main/kotlin/com/innoq/chainy/miner/Miner.kt
+++ src/main/kotlin/com/innoq/chainy/miner/Miner.kt
@@ -1,5 +1,6 @@
 package com.innoq.chainy.miner
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.innoq.chainy.model.Block
 import com.innoq.chainy.model.Transaction
 import java.security.MessageDigest
@@ -16,9 +17,21 @@ object Miner {
                 hashBlock(previous)
         )
 
+        // split json string to inject changing proof value (much faster than jackson)
+        val hash = jacksonObjectMapper().writer().writeValueAsString(seedBlock);
+        val start: Int = hash.indexOf("\"proof\":") + 8
+        val end: Int = start + hash.substring(start, hash.length).indexOf(",")
+        val prefix = hash.substring(0, start)
+        val postfix = hash.substring(end, hash.length)
+
         return generateSequence(seedBlock) { it.copy(proof = it.proof + 1) }
-                .dropWhile { !hashBlock(it).startsWith("0000") }
+                .dropWhile { !hashStringWithSha256(prefix + it.proof + postfix).startsWith("0000") }
                 .first()
+
+        // let the object serialize itself
+//        return generateSequence(seedBlock) { it.copy(proof = it.proof + 1) }
+//                .dropWhile { !hashBlock(it).startsWith("0000") }
+//                .first()
     }
 
     fun hashBlock(block: Block): String {
